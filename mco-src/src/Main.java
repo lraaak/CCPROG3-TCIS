595bcbc31015e66281bfd7ef1ff8088e612248e8
karl <mtd28123@gmail.com>
2025-07-30 20:03:47 +0800

    FINAL

--- mco-src/src/Main.java
+++ mco-src/src/Main.java
@@ -1,13 +1,5 @@
 import com.*;
 
-import com.Collection;
-import controller.MainMenuController;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.Parent;
-import javafx.stage.Stage;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
